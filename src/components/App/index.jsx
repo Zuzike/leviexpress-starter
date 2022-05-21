e352420509c78bd9f2b2f828ca7061a470166adb
Zuzana Nemesova <zuzana.nemesova@bearingpoint.com>
2022-05-21 10:53:01 +0200

    Kompo Reservation a spustenie routru

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Footer } from '../Footer';
 import { Header } from '../Header';
 import { Home } from '../Home';
+import { Reservation } from '../Reservation';
 
 export const App = () => (
-  <>
+  <BrowserRouter>
     <Header />
-    <Home />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/reservation" element={<Reservation />} />
+      {/*<Route
+        path="/bla"
+        element={
+          <main style={{ padding: '1rem' }}>
+            <p>There's nothing here!</p>
+          </main>
+        }
+      />*/}
+    </Routes>
     <Footer />
-  </>
+  </BrowserRouter>
 );
